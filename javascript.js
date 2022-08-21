82bc9f18a9a96db63c2822bddea46112dd7ad94e
Monito <phil.menard1668@gmail.com>
2022-08-21 10:00:19 -0300

    Add a color picker

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 
-let lengthGrid = 24;
+let lengthGrid = 12;
 let nbCases = lengthGrid * lengthGrid;
 
 for (let i = 0; i < nbCases; i++)   {   // create the divs
@@ -12,20 +12,29 @@ for (let i = 0; i < nbCases; i++)   {   // create the divs
 container.style.gridTemplateColumns = `repeat(${lengthGrid}, 1fr)`; // create a grill with the number of divs 
 
 const gridItems = document.getElementsByClassName("grid-item");
+const colorPicker = document.getElementById('colorpicker');
+let colorValue = colorPicker.value;
 const divs = Array.from(gridItems);
-let isMouseDown = false;
 
-// si mousedown => event qui turn ismousedown en true
-divs.forEach(div => div.addEventListener('mousedown', function(){
-    div.style.backgroundColor = "black";
-    divs.forEach(div => div.addEventListener('mousemove', colorizeCell));
-}));
+// if mouse is down, mouseover is activated
+container.addEventListener('mousedown', function(e){
+    e.target.style.backgroundColor = `${colorValue}`; // color the first cell 
+    divs.forEach(div => div.addEventListener('mouseover', colorizeCell));
+});
 
-// // si mousedown => event qui turn ismousedown en false
-divs.forEach(div => div.addEventListener('mouseup', function(){
-    divs.forEach(div => div.removeEventListener('mousemove', colorizeCell));
-}));
+// if mouse is up, mouseover is disactivated
+container.addEventListener('mouseup', function(){
+    divs.forEach(div => div.removeEventListener('mouseover', colorizeCell));
+});
 
 function colorizeCell(e) {
-    this.style.backgroundColor = "black";
+    this.style.backgroundColor = `${colorValue}`;
 }
+
+function changeColor(){
+    colorPicker.addEventListener('change', (e) => {
+        colorValue = e.target.value;
+    });
+};
+
+changeColor();
