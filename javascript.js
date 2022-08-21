bc7544210416dad6674f5b424f667851e1852061
Monito <phil.menard1668@gmail.com>
2022-08-21 12:15:51 -0300

    Add the clear button

--- javascript.js
+++ javascript.js
@@ -1,34 +1,44 @@
-const container = document.getElementById('container');
+const container = document.getElementById('grid');
+const gridItems = document.getElementsByClassName("grid-item");
+const divs = Array.from(gridItems);
+const colorPicker = document.getElementById('colorpicker');
+const clearDiv = document.getElementById('clear');
 
-let lengthGrid = 12;
+let colorValue = colorPicker.value;
+clearDiv.addEventListener('click', clearGrid);
+
+let mouseDown = false
+window.onmousedown = () => mouseDown = true;
+window.onmouseup = () => mouseDown = false;
+
+let lengthGrid = 10;
+let gridRatio = 2;
 let nbCases = lengthGrid * lengthGrid;
+let listColoredCells = [];
 
-for (let i = 0; i < nbCases; i++)   {   // create the divs
+for (let i = 0; i < nbCases * gridRatio; i++)   {   // create the divs
     let div = document.createElement('div');
     div.className = "grid-item";
     container.appendChild(div);
+    div.addEventListener('mousedown', colorizeCell)
+    div.addEventListener('mouseover', checkMouse)
+}
+container.style.gridTemplateColumns = `repeat(${lengthGrid * gridRatio}, 1fr)`; // create a grill with the number of divs 
+container.style.gridTemplateRows = `repeat(${lengthGrid}, 1fr)`;
+
+changeColor();
+
+function checkMouse(e)  {
+    if (mouseDown === true)  {
+        colorizeCell(e);
+    }
 }
 
-container.style.gridTemplateColumns = `repeat(${lengthGrid}, 1fr)`; // create a grill with the number of divs 
-
-const gridItems = document.getElementsByClassName("grid-item");
-const colorPicker = document.getElementById('colorpicker');
-let colorValue = colorPicker.value;
-const divs = Array.from(gridItems);
-
-// if mouse is down, mouseover is activated
-container.addEventListener('mousedown', function(e){
-    e.target.style.backgroundColor = `${colorValue}`; // color the first cell 
-    divs.forEach(div => div.addEventListener('mouseover', colorizeCell));
-});
-
-// if mouse is up, mouseover is disactivated
-container.addEventListener('mouseup', function(){
-    divs.forEach(div => div.removeEventListener('mouseover', colorizeCell));
-});
-
 function colorizeCell(e) {
-    this.style.backgroundColor = `${colorValue}`;
+    e.target.style.backgroundColor = `${colorValue}`;
+    e.target.classList.add("colored");
+    listColoredCells.push(e.target);
+    console.log(listColoredCells);
 }
 
 function changeColor(){
@@ -37,4 +47,14 @@ function changeColor(){
     });
 };
 
-changeColor();
+function clearGrid()    {
+    listColoredCells.forEach(div => div.style.backgroundColor = "beige");
+    console.log("working...");
+}
+
+// ajouter la classe "colored" quand une case a été colorée
+// on les ajoute dans une liste
+// quand on appuie sur la case "clear" => remettre background-color sur blue (pour tester, si fonctionne le placer sur "none")
+
+
+// on peut le faire ainsi : element.classList.add("my-class");
