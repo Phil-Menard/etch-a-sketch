15996cf44eef7fbd3785e5ed07e617c8d607d842
Monito <phil.menard1668@gmail.com>
2022-08-25 20:49:43 -0300

    Add style to the slider. Need polish.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,11 @@ const gridItems = document.getElementsByClassName("grid-item");
 const divs = Array.from(gridItems);
 const colorPicker = document.getElementById('colorpicker');
 const clearDiv = document.getElementById('clear');
+let slider = document.getElementById('sliderInput');
+let rangeValue = document.getElementById('rangeValue');
+
+console.log(slider);
+
 
 let colorValue = colorPicker.value;
 clearDiv.addEventListener('click', clearGrid);
@@ -11,22 +16,30 @@ let mouseDown = false
 window.onmousedown = () => mouseDown = true;
 window.onmouseup = () => mouseDown = false;
 
-let lengthGrid = 10;
-let gridRatio = 2;
-let nbCases = lengthGrid * lengthGrid;
-let listColoredCells = [];
 
-for (let i = 0; i < nbCases * gridRatio; i++)   {   // create the divs
-    let div = document.createElement('div');
-    div.className = "grid-item";
-    container.appendChild(div);
-    div.addEventListener('mousedown', colorizeCell)
-    div.addEventListener('mouseover', checkMouse)
-}
-container.style.gridTemplateColumns = `repeat(${lengthGrid * gridRatio}, 1fr)`; // create a grill with the number of divs 
-container.style.gridTemplateRows = `repeat(${lengthGrid}, 1fr)`;
+let listColoredCells = [];
+slider.addEventListener('click', setValueSlider);
 
 changeColor();
+createGrid();
+rangeValue.textContent = slider.value +" x "+ (slider.value*2);
+
+function createGrid()   {
+
+    let lengthGrid = slider.value;
+    let gridRatio = 2;
+    let nbCases = lengthGrid * lengthGrid;
+
+    for (let i = 0; i < nbCases * gridRatio; i++)   {   // create the divs
+        let div = document.createElement('div');
+        div.className = "grid-item";
+        container.appendChild(div);
+        div.addEventListener('mousedown', colorizeCell)
+        div.addEventListener('mouseover', checkMouse)
+    }
+    container.style.gridTemplateColumns = `repeat(${lengthGrid * gridRatio}, 1fr)`; // create a grill with the number of divs 
+    container.style.gridTemplateRows = `repeat(${lengthGrid}, 1fr)`;
+}
 
 function checkMouse(e)  {
     if (mouseDown === true)  {
@@ -49,12 +62,19 @@ function changeColor(){
 
 function clearGrid()    {
     listColoredCells.forEach(div => div.style.backgroundColor = "beige");
-    console.log("working...");
 }
 
-// ajouter la classe "colored" quand une case a été colorée
-// on les ajoute dans une liste
-// quand on appuie sur la case "clear" => remettre background-color sur blue (pour tester, si fonctionne le placer sur "none")
+function setValueSlider(e)   {
+    rangeValue.textContent = slider.value +" x "+ (slider.value*2);
+    while(container.firstChild) {   //supprime toutes les divs
+        container.removeChild(container.firstChild);
+    }
+    
+    createGrid(); //puis les reconstruit
+}
 
-
-// on peut le faire ainsi : element.classList.add("my-class");
+// Supprime tous les enfant d'un élément
+// var element = document.getElementById("top");
+// while (element.firstChild) {
+//   element.removeChild(element.firstChild);
+// }
